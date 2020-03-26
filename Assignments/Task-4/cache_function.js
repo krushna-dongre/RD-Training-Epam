f06361895cfc032e03a07103023ca8b6eed0be00
Krushna Dongre <krushna_dongre@epam.com>
2020-03-26 14:51:59 +0530

    Changes done as required

--- Assignments/Task-4/cache_function.js
+++ Assignments/Task-4/cache_function.js
@@ -5,7 +5,10 @@ function cache(func) {
     var memory = {};
     return function() {
         var key = JSON.stringify(arguments);
-        if (key in memory) return memory[key];
-        return memory[key] = func.apply(this, arguments);
+        if (key in memory){
+             return memory[key];
+        }
+        memory[key] = func.apply(this, arguments);
+        return memory[key];
     };
 }
--- Assignments/Task-5/array_func.js
+++ Assignments/Task-5/array_func.js
@@ -1,35 +1,45 @@
 // Array Helpers
 // http://www.codewars.com/kata/array-helpers
 
-Array.prototype.square = function() {
-    return this.map(function(x) {
-        return (x * x);
-    });
+if(Array.prototype.square){
+    Array.prototype.square = function() {
+        return this.map(function(x) {
+            return (x * x);
+        });
+    }
 }
-Array.prototype.cube = function() {
-    return this.map(function(x) {
-        return (x * x * x);
-    });
+if(Array.prototype.cube){
+    Array.prototype.cube = function() {
+        return this.map(function(x) {
+            return (x * x * x);
+        });
+    }
 }
-Array.prototype.sum = function() {
-    return this.reduce(function(s, v) {
-        return s + v;
-    }, 0);
+if(Array.prototype.sum){
+    Array.prototype.sum = function() {
+        return this.reduce(function(s, v) {
+            return s + v;
+        }, 0);
+    }
 }
-Array.prototype.average = function() {
-    return this.reduce(function(s, v) {
-        return (s + v);
-    }, 0) / this.length;
+if(Array.prototype.average){
+    Array.prototype.average = function() {
+        return this.reduce(function(s, v) {
+            return (s + v);
+        }, 0) / this.length;
+    }
 }
-
-Array.prototype.even = function() {
-    return this.filter(function(x) {
-        return x % 2 === 0;
-    });
+if(Array.prototype.even){
+    Array.prototype.even = function() {
+        return this.filter(function(x) {
+            return x % 2 === 0;
+        });
+    }
 }
-
-Array.prototype.odd = function() {
-    return this.filter(function(x) {
-        return x % 2 !== 0;
-    });
+if(Array.prototype.odd){
+    Array.prototype.odd = function() {
+        return this.filter(function(x) {
+            return x % 2 !== 0;
+        });
+    }
 }
